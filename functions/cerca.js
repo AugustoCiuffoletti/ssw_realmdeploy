c2932cbd3a051d8d9e379ff226f81798fb9b970a
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2022-05-17 18:18:26 +0200

    Correzione indentazione

--- functions/cerca.js
+++ functions/cerca.js
@@ -1,10 +1,9 @@
 exports = function(payload, response) {
-const {n} = payload.query;
-console.log("n=", n);
-var collection =
-context.services.get("mongodb-atlas").db("Meteo").collection("Citta");
-var t = collection.findOne({nome: n}).then(
-doc => doc.temperatura.toString()
-);
-return t;
+  const { n } = payload.query;
+  console.log("n=", n);
+  var collection =
+    context.services.get("mongodb-atlas").db("Meteo").collection("Citta");
+  var t = collection.findOne({ nome: n })
+	.then(	doc => doc.temperatura.toString() );
+  return t;
 };
--- functions/inserisci.js
+++ functions/inserisci.js
@@ -1,15 +1,17 @@
 exports = function(payload, response) {
-const {n} = payload.query;
-var collection =
-context.services.get("mongodb-atlas").db("Meteo").collection("Citta");
-var t = collection.count({nome: n})
-.then( (cnt) => {
-if ( cnt === 0 ) {
-var doc={"nome": n, temperatura: 0};
-collection.insertOne(doc);
-return doc;
-} else {
-return ("Gia' presente");
-} } );
-return t;
+  const { n } = payload.query;
+  var collection =
+    context.services.get("mongodb-atlas").db("Meteo").collection("Citta");
+  var t = collection.count( { nome: n } )
+    .then((cnt) => {
+      if (cnt === 0) {
+        var doc = { "nome": n, temperatura: 0 };
+        collection.insertOne(doc);
+        return doc;
+      }
+      else {
+        return ("Gia' presente");
+      }
+    });
+  return t;
 };
